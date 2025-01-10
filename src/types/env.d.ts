aa7abdbf31a05b1ccc53e5a4ba9978820e4562c2
Carlos Nardo <c.cesar.nardo@gmail.com>
2025-01-10 16:03:03 -0300

    chore: s3 implementation
    (cherry picked from commit 8f4824845d28df8fc5238ac67bbc3962288128bb)

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -5,6 +5,6 @@ declare namespace NodeJS {
     NEXT_PUBLIC_AWS_ACCESS_KEY_ID: string;
     NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY: string;
     NEXT_PUBLIC_AWS_REGION: string;
-    NEXT_PUBLIC_AWS_S3_BUCKET_NAME: string;
+    NEXT_PUBLIC_S3_BUCKET_NAME: string;
   }
 }
